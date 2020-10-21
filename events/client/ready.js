dedf5ea1aeead2285534cdfb0939406b0dca478e
FC5570 <68158483+FC5570@users.noreply.github.com>
2020-10-21 12:29:49 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -8,7 +8,7 @@ module.exports = async bot => {
   { useNewUrlParser: true, useUnifiedTopology: true }
 
 );
-    console.log(`${bot.user.username} is available now!`)
+    console.log(`${bot.user.username} is now online!`)
     let totalUsers = bot.guilds.cache.reduce((acc, value) => acc + value.memberCount, 0)
     var activities = [ `${bot.guilds.cache.size} servers`, `${totalUsers} users!` ], i = 0;
     setInterval(() => bot.user.setActivity(`${PREFIX}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }),5000)
