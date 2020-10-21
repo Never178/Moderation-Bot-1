2b811f8d5643640502fd31899d7926ab22a75c07
FC5570 <68158483+FC5570@users.noreply.github.com>
2020-10-21 12:04:41 +0530

    Create ready.js

---
+++ events/client/ready.js
@@ -0,0 +1,16 @@
+const { PREFIX } = require('../../config');
+const mongoose = require('mongoose')
+module.exports = async bot => {
+    mongoose.connect(
+
+  "mongodb+srv://username:password@cluster0.e0cnl.mongodb.net/<dbname>?retryWrites=true&w=majority", //your mongo dB url
+
+  { useNewUrlParser: true, useUnifiedTopology: true }
+
+);
+    console.log(`${bot.user.username} is available now!`)
+    let totalUsers = bot.guilds.cache.reduce((acc, value) => acc + value.memberCount, 0)
+    var activities = [ `${bot.guilds.cache.size} servers`, `${totalUsers} users!` ], i = 0;
+    setInterval(() => bot.user.setActivity(`${PREFIX}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }),5000)
+    
+};
