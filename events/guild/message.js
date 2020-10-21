a483065bb65bd7621aeb1ba52cbf5b62c0921574
FC5570 <68158483+FC5570@users.noreply.github.com>
2020-10-21 12:06:25 +0530

    Create message.js

---
+++ events/guild/message.js
@@ -0,0 +1,27 @@
+const db = require('quick.db');
+const { PREFIX } = require('../../config');
+
+module.exports = async (bot, message) => {
+    try {
+        if (message.author.bot || message.channel.type === "dm") return;
+
+        let prefix;
+        let fetched = await db.fetch(`prefix_${message.guild.id}`);
+
+        if (fetched === null) {
+            prefix = PREFIX
+        } else {
+            prefix = fetched
+        }
+
+        let args = message.content.slice(prefix.length).trim().split(/ +/g);
+        let cmd = args.shift().toLowerCase();
+
+        if (!message.content.startsWith(prefix)) return;
+
+        var commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
+        if (commandfile) commandfile.run(bot, message, args, ops)
+    } catch (e) {
+        console.log(e);
+    }
+}
